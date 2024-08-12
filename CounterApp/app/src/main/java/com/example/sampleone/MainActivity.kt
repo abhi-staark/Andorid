9f4769dc7a7cabcb84864db9524c1714dd500b15
AbhishekpYoungsoft <127491790+AbhishekpYoungsoft@users.noreply.github.com>
2024-08-12 17:23:22 +0530

    Counter app

---
+++ CounterApp/app/src/main/java/com/example/sampleone/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.sampleone
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        var counter =0
+        val textValue = findViewById<TextView>(R.id.textId1)
+        textValue.text=counter.toString()
+        val button = findViewById<Button>(R.id.counterBtn)
+        button.setOnClickListener(
+             View.OnClickListener(){
+                 counter ++
+                 textValue.text = (counter.toString())
+                Log.i("button","Button clicked")
+            }
+        )
+    }
+}
